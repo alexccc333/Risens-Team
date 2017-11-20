d7fad4aa06f75599a51a2581067987dbd03142e1
Kriegsmarine13 <rediSKA195@mail.ru>
2017-11-21 01:25:01 +0300

    API more fixes
    -переменная занесена в константы
    -добавлен конструктор $mysqli
    -пробелы убраны

--- api/API.php
+++ api/API.php
@@ -3,6 +3,11 @@
 class API {
 
     protected $_mysqli = null;
+    const TABLE_NAME_URI = 2;
+
+    public function __construct($mysqli){
+        $this->_mysqli = $mysqli;
+    }
 
     //Смотрим на URI и получаем его для нужд Высшего Блага
     public function getURI(){
@@ -12,13 +17,9 @@ class API {
 
     //Метод для вызова
     public function getRequest($id){
-        $tableNameUri = 2;  //Объявляем элемент ссылки, отвечающий за таблицу в запросе
         $uri = $this->getURI();  //Смотрим на uri
         $table = explode('/', $uri); //делим на куски
-        $table = $table[$tableNameUri]; // берём тот, который укажет на таблицу
-        include_once('../bd.php'); //подрубаемся к бд
-        $this->_mysqli = $mysqli;
-
+        $table = $table[self::TABLE_NAME_URI]; // берём тот, который укажет на таблицу
 
         //Определяем таблицу для запроса
         switch($table){
@@ -48,10 +49,7 @@ class API {
             }
             $result->free();
 
-
             return json_encode($returnArray);
         }
-
     }
-
 }
--- api/index.php
+++ api/index.php
@@ -1,11 +1,10 @@
 <?php
 include('API.php');
-$api = new API;
-
+include('../bd.php');
+$api = new API($mysqli);
 
 if(preg_match("^/api/(anime|manga)/[0-9]+^", $api->getURI())){
     $id = explode('/', $api->getURI());
     $id = array_pop($id);
     echo $api->getRequest($id);
-
 }
